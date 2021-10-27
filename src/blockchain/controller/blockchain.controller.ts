12211051c0743a2aa414238a3a83d48b2dcbf048
Kamil Kaczor <kkaczor@concisesoftware.com>
2021-10-27 14:30:27 +0200

    blockchain functionality provided

--- src/blockchain/controller/blockchain.controller.ts
+++ src/blockchain/controller/blockchain.controller.ts
@@ -1,7 +1,8 @@
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Body, Controller, Post, Get, Param } from '@nestjs/common';
-import { BlockchainDTO } from '../dto/blockchain.dto';
 import { BlockchainService } from '../service/blockchain.service';
+import { BlockchainDTO } from '../dto/blockchain.dto';
+import { BlockchainType } from '../type/blockchain.type';
 
 @ApiTags('blockchain')
 @Controller('blockchain')
@@ -11,10 +12,19 @@ export class BlockchainController {
   @ApiOperation({ summary: 'initializes new blockchain operations' })
   @ApiResponse({ status: 201 })
   @Post('')
-  async init(@Body() data: BlockchainDTO): Promise<object> {
+  async init(@Body() data: BlockchainDTO): Promise<BlockchainType> {
     this.blockchainService.initAccountBalances(data.initialAccountBalances);
 
-    return { success: true };
+    const groupedTransactions = this.blockchainService.groupTransactions(
+      data.pendingTransactions,
+      data.blockSize,
+    );
+
+    for (const transactions of groupedTransactions) {
+      this.blockchainService.processTransactions(transactions);
+    }
+
+    return { blockchain: this.blockchainService.getBlockchain() };
   }
 
   @ApiOperation({ summary: 'returns blockchain accounts balance' })
---
+++ src/blockchain/type/block.type.ts
@@ -0,0 +1,5 @@
+export interface BlockType {
+  hash: string;
+  previousHash: string;
+  nonce: number;
+}
